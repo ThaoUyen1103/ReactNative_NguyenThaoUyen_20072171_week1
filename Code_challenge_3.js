a58cfe634e98774ade9c94549753c5e682b2057f
unknown <nguyenthaouyen0165@gmai.com>
2024-08-23 09:15:24 +0700

    week1

---
+++ Code_challenge_3.js
@@ -0,0 +1,43 @@
+/*Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test for that 
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+both teams have the same score and both have a score greater or equal 100 
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+var Dolphins_Score = [97, 112, 101];
+var Koalas_Score = [109, 95, 123];
+
+
+var dolphinsAvg = (Dolphins_Score[0] + Dolphins_Score[1] + Dolphins_Score[2]) / 3;
+var koalasAvg = (Koalas_Score[0] + Koalas_Score[1] + Koalas_Score[2]) / 3;
+
+
+var reqScoreDol = dolphinsAvg >= 100;
+var reqScoreKoa = koalasAvg >= 100;
+
+console.log("Coding Challenge #3");
+
+if (dolphinsAvg > koalasAvg && reqScoreDol) {
+    console.log("Team Dolphins win!", dolphinsAvg.toFixed(1), ">", koalasAvg);
+} else if (koalasAvg > dolphinsAvg && reqScoreKoa) {
+    console.log("Team Koalas win!", dolphinsAvg.toFixed(1), "<", koalasAvg);
+} else if (dolphinsAvg === koalasAvg && reqScoreDol && reqScoreKoa) {
+    console.log("Draw", dolphinsAvg.toFixed(1), "=", koalasAvg);
+} else {
+    console.log("No team win");
+}
